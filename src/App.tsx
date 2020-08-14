c3ca82f20badc325b8d0d3118b3243407b7cf16e
Ivan <ivan@Ivans-MacBook-Pro.local>
2020-08-14 12:28:29 -0700

    init app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      HI!
+      <h3>Qs</h3>
+      <h3>Factions</h3>
+      <h3>Items</h3>
+    </div>
+  );
+}
+
+export default App;
