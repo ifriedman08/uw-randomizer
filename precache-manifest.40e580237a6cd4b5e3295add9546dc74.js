1f170e4b9577839b0390736dede25b54d80e9391
Ivan <ivan@Ivans-MacBook-Pro.local>
2020-08-23 01:31:33 -0700

    Updates

---
+++ precache-manifest.40e580237a6cd4b5e3295add9546dc74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65abe0f58151ceb310c3b0daafb2f871",
+    "url": "/uw-randomizer/index.html"
+  },
+  {
+    "revision": "c7ae6f04263783231775",
+    "url": "/uw-randomizer/static/css/main.d0e37305.chunk.css"
+  },
+  {
+    "revision": "c055f44aa1ce8c84bdb2",
+    "url": "/uw-randomizer/static/js/2.7218c8cf.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/uw-randomizer/static/js/2.7218c8cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ae6f04263783231775",
+    "url": "/uw-randomizer/static/js/main.a43b481c.chunk.js"
+  },
+  {
+    "revision": "0b1a29dbc15dd854312b",
+    "url": "/uw-randomizer/static/js/runtime-main.a1aa81a9.js"
+  }
+]);
