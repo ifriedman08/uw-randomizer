69e380a4292916bf627a7df3ffcdaa05559a702e
Ivan <ivan@Ivans-MacBook-Pro.local>
2020-08-29 12:17:11 -0700

    added alt name gen link

--- src/sections/names/Names.tsx
+++ src/sections/names/Names.tsx
@@ -1,16 +1,31 @@
 import React, { FC } from "react";
 import styled from "styled-components";
+import { Button } from "@material-ui/core";
 
 const IFrame = styled.iframe`
   position: absolute;
   left: 0;
   bottom: 0;
   width: 100%;
-  height: 85%;
+  height: 80%;
   border: 2px;
 `;
 const Names: FC = () => {
-  return <IFrame src="https://www.fantasynamegenerators.com/" />;
+  return (
+    <>
+      <Button
+        variant="text"
+        color="default"
+        size="large"
+        onClick={() =>
+          window.open("https://donjon.bin.sh/scifi/name/", "_blank")
+        }
+      >
+        Link to generator
+      </Button>
+      <IFrame src="https://www.fantasynamegenerators.com/" />
+    </>
+  );
 };
 
 export default Names;
