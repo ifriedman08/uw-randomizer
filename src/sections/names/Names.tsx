68fa0794232b9924cd91e1fae4e43742531620d1
Ivan <ivan@Ivans-MacBook-Pro.local>
2020-08-28 18:00:18 -0700

    app and name iframe styles updated

--- src/sections/names/Names.tsx
+++ src/sections/names/Names.tsx
@@ -1,6 +1,14 @@
 import React, { FC } from "react";
-import { IFrame } from "../../components";
+import styled from "styled-components";
 
+const IFrame = styled.iframe`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 85%;
+  border: 2px;
+`;
 const Names: FC = () => {
   return <IFrame src="https://www.fantasynamegenerators.com/" />;
 };
