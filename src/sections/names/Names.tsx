c060370036335adfbfd7eb855b6cc81f5b0b0bb9
Ivan <ivan@Ivans-MacBook-Pro.local>
2020-09-11 22:26:06 -0700

    names to button

--- src/sections/names/Names.tsx
+++ src/sections/names/Names.tsx
@@ -1,29 +1,41 @@
 import React, { FC } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { Button } from "@material-ui/core";
 
-const IFrame = styled.iframe`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  height: 75%;
-  border: 2px;
-`;
+// const IFrame = styled.iframe`
+//   position: absolute;
+//   left: 0;
+//   bottom: 0;
+//   width: 100%;
+//   height: 75%;
+//   border: 2px;
+// `;
 const Names: FC = () => {
   return (
     <>
       <Button
         variant="text"
         color="default"
-        size="large"
+        size="medium"
         onClick={() =>
           window.open("https://donjon.bin.sh/scifi/name/", "_blank")
         }
       >
-        Link to generator
+        Link to Donjon
       </Button>
-      <IFrame src="https://www.fantasynamegenerators.com/" />
+      <Button
+        variant="text"
+        color="default"
+        size="medium"
+        onClick={() =>
+          window.open(
+            "https://www.fantasynamegenerators.com/futuristic-names.php",
+            "_blank"
+          )
+        }
+      >
+        Fantasy Name Generator
+      </Button>
     </>
   );
 };
